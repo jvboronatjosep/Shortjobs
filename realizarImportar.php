521d59f6222ed61671f068383ac655303ba7a2f3
Daniel Aguado <daagro@floridauniversitaria.es>
2024-05-14 12:28:39 +0200

    Importar Final

--- realizarImportar.php
+++ realizarImportar.php
@@ -3,11 +3,11 @@
 require_once 'autoloader.php'; 
 
 $confFile = "./conf.csv";
-//$importEmpresa = "empresas.csv";
-//$importUsuarios = "usuarios.csv";
+$importEmpresa = "empresas.csv";
+$importUsuarios = "usuarios.csv";
 $importTrabajos = "trabajos.csv";
 $importador = new Importar($confFile); 
-//$importadorEmpresa->importarEmpresa($importEmpresa);
-//$importador->importarUsuarios($importUsuarios);
-$importador->importarTrabajos($importTrabajos); // Corrección aquí
+$importadorEmpresa->importarEmpresa($importEmpresa);
+$importadorUsuriaos->importarUsuarios($importUsuarios);
+$importadorTrabajos->importarTrabajos($importTrabajos); // Corrección aquí
 header('Location: ./Inicio.php');
