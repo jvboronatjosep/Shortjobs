f4ed2a2df0c15117b74d42520a6dbce81dd64b6c
jvboronatjosep <jvboronatdavid@gmail.com>
2024-05-16 09:10:10 +0200

    solucionando error en el nav

--- inicio2.php
+++ inicio2.php
@@ -7,9 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style2.css">
+    <style>
+    </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top mt-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,10 +32,9 @@
                         <a class="nav-link text-white" href="temporadas.html" style="margin-right: 30px;">Contacto</a>
                     </li>
                 </ul>
-
                 <div class="header-buttons">
-                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block" style="margin-right: 20px;">Iniciar Sesión</a>
-                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block" style="margin-right: 60px;">Registro</a>
+                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block" style="margin-right: 5px;">Iniciar Sesión</a>
+                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block">Registro</a>
                 </div>
             </div>
         </div>
@@ -42,13 +43,13 @@
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
-                <img src="img/adult-1866533_1920.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/adult-1866533_1920.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -61,14 +62,16 @@
         </button>
     </div>
 
+    <div class="container mt-5">
         <h2>A TU ALCANCE</h2>
-    <h1>Encuentra trabajos cortos</h1>
-    <p>¡Encuentra trabajos temporales de forma rápida y segura con ShortJobs! Conectamos a 
-    empresas con necesidades de personal temporal y a trabajadores disponibles para hacer trabajos por horas o días. 
-    ¡Es la solución perfecta para cubrir tus necesidades laborales de manera flexible y conveniente!</p>
-    
-    <form action="">
-    <input type="submit" value="descubrelo" id="enviar">
-    </form>
+        <h1>Encuentra trabajos cortos</h1>
+        <p>¡Encuentra trabajos temporales de forma rápida y segura con ShortJobs! Conectamos a 
+        empresas con necesidades de personal temporal y a trabajadores disponibles para hacer trabajos por horas o días. 
+        ¡Es la solución perfecta para cubrir tus necesidades laborales de manera flexible y conveniente!</p>
+        
+        <form action="">
+            <input type="submit" value="descubrelo" id="enviar">
+        </form>
+    </div>
 </body>
 </html>
