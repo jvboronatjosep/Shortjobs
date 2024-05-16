fbe657fdaf82e0439009419775697324f7d6da5d
Carlos Fernandez <cafebo@floridauniversitaria.es>
2024-05-16 09:45:19 +0200

    arreglar pagina inicio.php

--- Inicio.php
+++ Inicio.php
@@ -1,80 +1,91 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - ShortJobs</title>
     <link rel="icon" href="img/LogoShortJobs-removebg-preview.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style2.css">
     <style>
-
     </style>
 </head>
-
 <body>
 
-    <header>
-        <div class="logo">
-            <a href="Inicio.php"><img src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" href="inicio.php" style="margin-right: 15px;">Inicio </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link text-white" href="Trabajos.php" style="margin-right: 15px;">Trabajos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="Calendario.php" style="margin-right: 15px;">Calendario</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="Destacadas.php" style="margin-right: 15px;">Destacadas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="Contacto.php" style="margin-right: 30px;">Contacto</a>
+                    </li>
+                </ul>
+                <div class="header-buttons">
+                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block" style="margin-right: 5px;">Iniciar Sesión</a>
+                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg btn-block">Registro</a>
+                </div>
+            </div>
         </div>
-        <nav class="botones">
-            <a class="boton" href="Inicio.php">Inicio</a>
-            <a class="boton" href="Trabajos.php">Trabajos</a>
-            <a class="boton" href="Calendario.php">Calendario</a>
-            <a class="boton" href="Destacadas.php">Destacadas</a>
-            <a class="boton" href="Contacto.php">Contacto</a>
-        </nav>
-        <div class="header-buttons">
-            <a href="Empresa-Usuario.php">Iniciar Sesión</a>
-            <a href="Empresa-Usuario.php">Registro</a>
-        </div>
-
-        
-    </header>
+    </nav>
+    
 
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
+        <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
-                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/adult-1866533_1920.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item" data-bs-interval="2000">
-                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="img/adult-1866533_1920.jpg" class="d-block w-100 carousel-image" alt="...">
+                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100" alt="...">
             </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
-            </button>
-        </div>
+        </button>
+    </div>
 
-
-    <h2>A TU ALCANCE</h2>
-    <h1>Encuentra trabajos cortos</h1>
-    <p>¡Encuentra trabajos temporales de forma rápida y segura con ShortJobs! Conectamos a 
-    empresas con necesidades de personal temporal y a trabajadores disponibles para hacer trabajos por horas o días. 
-    ¡Es la solución perfecta para cubrir tus necesidades laborales de manera flexible y conveniente!</p>
-    
-    <form action="">
-    <input type="submit" value="descubrelo" id="enviar">
-    </form>
+    <div class="container mt-5">
+        <h2>A TU ALCANCE</h2>
+        <h1>Encuentra trabajos cortos</h1>
+        <p>¡Encuentra trabajos temporales de forma rápida y segura con ShortJobs! Conectamos a 
+        empresas con necesidades de personal temporal y a trabajadores disponibles para hacer trabajos por horas o días. 
+        ¡Es la solución perfecta para cubrir tus necesidades laborales de manera flexible y conveniente!</p>
+        
+        <form action="">
+            <input type="submit" value="descubrelo" id="enviar">
+        </form>
+    </div>
 </body>
-
-
-
 <div class="contacto"><footer class="pie-pagina">
     <div class="grupo-1">
         <div class="Caja">
-                <a href=""><img src="" alt=""></a>
+        <a href="Inicio.php"><img src="img/LogoShortJobs-removebg-preview.png" alt="" width="200px"></a>
+
         </div>
         <div class="Caja">
             <div class="Medio">
@@ -99,4 +110,5 @@
         <small>&copy; 2024 <b>ShortJobs</b> - Todos Los Derechos Reservados.</small>
     </div>
 </footer></div>
+
 </html>
