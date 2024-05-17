96c0a196ee6533719e1241405beebfd688b6ed59
Carlos Fernandez <cafebo@floridauniversitaria.es>
2024-05-17 12:01:33 +0200

    arreglando

--- shortJobs.sql
+++ shortJobs.sql
@@ -1,12 +1,10 @@
--- Elimina la base de datos si existe y la crea nuevamente
-DROP DATABASE IF EXISTS shortJobs;
-CREATE DATABASE IF NOT EXISTS shortJobs;
+drop database if exists shorJobs;
+create database if not exists shortJobs;
 
--- Usa la base de datos recién creada
-USE shortJobs;
+use shortJobs;
 
--- Elimina la tabla 'usuarios' si existe y la crea nuevamente
-DROP TABLE IF EXISTS usuarios;
+
+drop table if exists usuarios;
 
 CREATE TABLE usuarios (
     dni VARCHAR(9) PRIMARY KEY,
@@ -15,10 +13,16 @@ CREATE TABLE usuarios (
     curriculum VARCHAR(255),
     direccion VARCHAR(100),
     ciudad VARCHAR(30),
-    correoElectronico VARCHAR(50),
+    correoElectronico VARCHAR(30),
     nombreUsuario VARCHAR(50),
     contraseña VARCHAR(100) 
-);
+    userId int(11) NOT NULL AUTO_INCREMENT,
+  userName varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  userPassword varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  securePassword varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (userId),
+  UNIQUE KEY userName (userName)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 
 drop table if exists empresas;
@@ -27,7 +31,13 @@ create table empresas(
     tema varchar (100),
     nombre varchar (50),
     sedes varchar (255)
-);
+        empresaId int(11) NOT NULL AUTO_INCREMENT,
+  empresaName varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  empresaPassword varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  securePassword varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (empresaId),
+  UNIQUE KEY empresaName (empresaName)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 drop table if exists trabajos;
 create table trabajos(
@@ -42,33 +52,9 @@ create table trabajos(
 );
 
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `userPassword` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `securePassword` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `userName` (`userName`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Elimina la tabla 'trabajos' si existe y la crea nuevamente
-DROP TABLE IF EXISTS trabajos;
 
-CREATE TABLE trabajos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(50),
-    empresa VARCHAR(50),
-    descripcion VARCHAR(255),
-    ubicacion VARCHAR(50),
-    fecha DATE,
-    salario DECIMAL(10, 2),
-    duracion DECIMAL(10, 2)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
--- Inserta un registro en la tabla 'empresas'
 INSERT INTO empresas (empresaId, empresaName, empresaPassword, securePassword) VALUES
 (1, 'TerraNova', '1234', '$2y$10$L8H6tTdwYbrxE32W3oOg/.u/wy9rcxFOryh3Z92LLd6UtN1fZ8Ad.');
-
--- Confirma la transacción
 COMMIT;
