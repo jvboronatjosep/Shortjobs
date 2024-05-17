e7a29c3c76fefa4f6a2163fea5f230d782741f9c
jvboronatjosep <jvboronatdavid@gmail.com>
2024-05-17 17:53:56 +0200

    cambios frontend

--- trabajos_trabajador.php
+++ trabajos_trabajador.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style_trabajos.css">
     <style>
     </style>
 </head>
@@ -47,27 +47,11 @@
     </nav>
     
 
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
-                <img src="img/adult-1866533_1920.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+
+<div id="hero-text" style="height: 600px; background-color: black; display: flex; align-items: center; justify-content: center;">
+    <h2 style="text-align: center; color: white; font-size: 50px">Desbloquea tu potencial laboral con ShortJobs:<br>Encuentra, conecta, ¡y empieza a trabajar!</h2>
+</div>
+
 
 
 
@@ -81,6 +65,7 @@
         </div>
     </div>
 </div>
+
 
 
 
