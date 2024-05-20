122eb3ec9aede8993c01249d60db21b29e264308
jvboronatjosep <jvboronatdavid@gmail.com>
2024-05-20 08:48:10 +0200

    arregalando inicio

--- Inicio.php
+++ Inicio.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShortJobs</title>
-    <link rel="icon" href="img/LogoShortJobs-removebg-preview.png" type="image/x-icon">
+    <title>Inicio - ShortJobs</title>
+    <link rel="icon" href="img/LogoShortJobs.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
@@ -16,7 +16,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
+            <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -47,64 +47,78 @@
     </nav>
     
 
-    <div class="carousel-item">
-        <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100" alt="...">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="img/adult-1866533_1920.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="img/ai-generated-8593076_1280.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/workshop-4524838_1920.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
+
 
 
 <div class="container-fluid py-5 d-flex justify-content-center align-items-center text-dark bg-light" style="height: 600px;">
     <div class="row">
         <div class="col-sm-6 text-center">
-            <h3 class="fw-bold" style="font-size: 55px; margin-right: 30px;">Empresa especializada<br>en unir talento <br>temporal con trabajos flexibles</h3>
+            <h3 class="fw-bold" style="font-size: 65px;">Empresa especializada <br> en unir talento <br> temporal con <br> trabajos flexibles</h3>
         </div>
         <div class="col-sm-6 text-start align-self-center">
-            <h5 class="text-justify" style="font-size: 25px; margin-left: 30px;">Ofrecemos soluciones completas para trabajadores<br>temporales y empresas,desde selección de <br> talento hasta gestión de proyectos,<br> adaptándonos al mercado laboral <br>cambiante para garantizar el éxito. <br> <br><a href="">Leer más</a></h5>
+            <h5 class="text-justify" style="font-size: 30px;">Ofrecemos soluciones completas para trabajadores temporales y empresas, desde selección de talento hasta gestión de proyectos, adaptándonos al mercado laboral cambiante para garantizar el éxito. <br> <br><a href="">Leer más</a></h5>
         </div>
     </div>
 </div>
 
 
 
+
+
+    
+
 </body>
 
-<footer class="bg-dark text-light py-4">
-    <div class="container">
-        <div class="row text-center text-md-left">
-            <div class="col-md-4 mb-4 mb-md-0">
-                <a href="Inicio.php">
-                    <img src="img/LogoShortJobs-removebg-preview.png" alt="ShortJobs Logo" width="150">
-                </a>
-            </div>
-            <div class="col-md-4 mb-4 mb-md-0">
-                <h5>Contacto</h5>
-                <p>Teléfono: 679 63 78 85</p>
-            </div>
-            <div class="col-md-4">
-                <h5>Síguenos</h5>
-                <div class="d-flex justify-content-center justify-content-md-center">
-                    <a href="https://www.facebook.com/floridauniversitaria" class="text-light mx-2" aria-label="Facebook">
-                        <i class="fab fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="https://www.instagram.com/florida_uni/" class="text-light mx-2" aria-label="Instagram">
-                        <i class="fab fa-instagram fa-lg"></i>
-                    </a>
-                    <a href="https://twitter.com/floridauni" class="text-light mx-2" aria-label="Twitter">
-                        <i class="fab fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/school/florida-universitaria/?originalSubdomain=es" class="text-light mx-2" aria-label="LinkedIn">
-                        <i class="fab fa-linkedin fa-lg"></i>
-                    </a>
-                    <a href="https://www.youtube.com/c/floridauniversitaria" class="text-light mx-2" aria-label="YouTube">
-                        <i class="fab fa-youtube fa-lg"></i>
-                    </a>
-                </div>
+
+
+<div class="contacto"><footer class="pie-pagina">
+    <div class="grupo-1">
+        <div class="Caja">
+        <a href="Inicio.php"><img src="img/LogoShortJobs.png" alt="" width="200px"></a>
+
+        </div>
+        <div class="Caja">
+            <div class="Medio">
+                <h2 id="contacto">Contacto:</h2>
+                <p>Telefono De Contacto:</p>
+                <p>☎️ 679 63 78 85</p>
             </div>
         </div>
-        <div class="row mt-4">
-            <div class="col text-center">
-                <small>&copy; 2024 <b>ShortJobs</b> - Todos Los Derechos Reservados.</small>
+        <div class="Caja">
+            <h2>Siguenos:</h2>
+            <div class="red-social">
+                <a href="https://www.facebook.com/floridauniversitaria" class="fa fa-facebook"></a>
+                <a href="https://www.instagram.com/florida_uni/" class="fa fa-instagram"></a>
+                <a href="https://twitter.com/floridauni" class="fa fa-twitter"></a>
+                <a href="https://www.linkedin.com/school/florida-universitaria/?originalSubdomain=es"
+                    class="fa fa-linkedin"></a>
+                <a href="https://www.youtube.com/c/floridauniversitaria" class="fa fa-youtube"></a>
             </div>
         </div>
     </div>
-</footer>
+    <div class="grupo-2">
+        <small>&copy; 2024 <b>ShortJobs</b> - Todos Los Derechos Reservados.</small>
+    </div>
+</footer></div>
 </html>
