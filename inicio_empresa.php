fb11951210e79541eb8e455e203727059c549529
jvboronatjosep <jvboronatdavid@gmail.com>
2024-05-20 12:56:13 +0200

    inicios acabasdos, style carpeta

--- inicio_empresa.php
+++ inicio_empresa.php
@@ -8,38 +8,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style_inicio_empresa.css">
+    <link rel="stylesheet" href="style/style_inicio_empresa.css">
     <style>
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation" style="background-color: white;">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+
+        <div class="collapse navbar-collapse" id="menu">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link text-white active" href="inicio.php" style="margin-right: 15px;">Inicio </a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link text-white" href="Trabajos.php" style="margin-right: 15px;">Mis Trabajos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="Calendario.php" style="margin-right: 15px;">Calendario</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="Destacadas.php" style="margin-right: 15px;">Trabajadors Destacados</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="Contacto.php" style="margin-right: 30px;">Contacto</a>
-                    </li>
-                </ul>
-            </div>
+                <li class="nav-item">
+                    <a class="nav-link text-white active" href="inicio.php" style="margin-right: 15px;">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Trabajos.php" style="margin-right: 15px;">Mis Trabajos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Destacadas.php" style="margin-right: 15px;">Trabajadores Destacados</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Contacto.php" style="margin-right: 30px;">Contacto</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
     
     <div>
         <div class="img_principal">
