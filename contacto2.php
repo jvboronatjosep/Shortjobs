385631cc26cdf1b482cd6d4c2526da19c648100c
jvboronatjosep <jvboronatdavid@gmail.com>
2024-05-21 08:50:44 +0200

    contacto front

--- contacto2.php
+++ contacto2.php
@@ -8,13 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style/style_inicio_trabajador.css">
+    <link rel="stylesheet" href="style/style_contacto.css">
 </head>
-<body>
+<body  style=" background-color: #E0FFFF;">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
     <div class="container-fluid">
-        <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
+        <a class="navbar-brand" href="inicio.php">
+        <img class="ml-4" style="height: 80px; width: 130px; margin-left: 40px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
         <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation" style="background-color: white;">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -23,7 +24,7 @@
         <div class="collapse navbar-collapse" id="menu">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link text-white active" href="inicio.php" style="margin-right: 15px;">Inicio</a>
+                    <a class="nav-link text-white" href="inicio.php" style="margin-right: 15px;">Inicio</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="Trabajos.php" style="margin-right: 15px;">Trabajos</a>
@@ -35,7 +36,7 @@
                     <a class="nav-link text-white" href="Destacadas.php" style="margin-right: 15px;">Destacadas</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="Contacto.php" style="margin-right: 30px;">Contacto</a>
+                    <a class="nav-link text-white active" href="Contacto.php" style="margin-right: 30px;">Contacto</a>
                 </li>
             </ul>
         </div>
@@ -45,14 +46,14 @@
     
     <div>
         <div class="img_principal">
-            <img src="img/foto_inicio_contacto.jpg" class="d-block w-100" alt="...">
+            <img src="img/img_inicio_contacto.jpg" class="d-block w-100" alt="...">
         </div>
     </div>
 
-    <body>
 
-<div class="container">
-    <h2>Formulario de Contacto</h2>
+
+<div class="container w-100 mb-5">
+    <h2 class="mt-5 d-flex justify-content-center">Formulario de Contacto</h2>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nombre = htmlspecialchars($_POST['nombre']);
@@ -64,8 +65,8 @@
         $cuerpoMensaje = "Nombre: $nombre\nCorreo: $email\n\nMensaje:\n$mensaje";
         
         $cabeceras = 'From: ' . $email . "\r\n" .
-                     'Reply-To: ' . $email . "\r\n" .
-                     'X-Mailer: PHP/' . phpversion();
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
         
         if (mail($para, $asunto, $cuerpoMensaje, $cabeceras)) {
             echo '<p class="success">Mensaje enviado correctamente.</p>';
@@ -74,22 +75,24 @@
         }
     }
     ?>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form class="form-register" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <h4>Formulario de Registro</h4>
+        
         <label for="nombre">Nombre</label>
-        <input type="text" id="nombre" name="nombre" required>
+        <input type="text" id="nombre" name="nombre" class="controls" required>
         
         <label for="email">Correo Electrónico</label>
-        <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email" class="controls" required>
         
         <label for="mensaje">Mensaje</label>
-        <textarea id="mensaje" name="mensaje" required></textarea>
+        <textarea id="mensaje" name="mensaje" class="controls" required></textarea>
         
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar" class="botons">
     </form>
 </div>
 
 </body>
-</body>
+
 <footer class="pie-pagina">
         <div class="grupo-1">
             <div class="Caja">
