0e4a8eefdfec37d8fa6a3bf9fdf22a1b4d95a83f
Daniel Aguado <daagro@floridauniversitaria.es>
2024-05-21 11:32:07 +0200

    hacer frotn y arrreglar formulario crear trabajo

--- perfilEmpresa.php
+++ perfilEmpresa.php
@@ -19,82 +19,119 @@ $nombres = $gestion->getEmpresaByName($userName);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil de Empresa</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Perfil de Empresa - ShortJobs</title>
+    <link rel="icon" href="img/LogoShortJobs.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c154837196.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/style_inicio_empresa.css">
     <style>
-        .navbar-dark .navbar-nav .nav-link {
+        .navbar-nav .nav-link {
             color: white;
         }
-        
-       
-        .navbar-dark .navbar-brand img {
-            filter: brightness(0) invert(1);
+        .navbar-brand img {
+            height: 80px;
+            width: 130px;
+            margin-left: 40px;
         }
-
-        .header-buttons .btn-light {
+        .btn-light {
             color: black;
+        }
+        .btn-light:hover {
+            color: white;
+            background-color: #343a40;
         }
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="inicio.php"><img src="img/LogoShortJobs-removebg-preview.png" alt="ShortJobs Logo"></a>
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="inicio.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Trabajos.php">Mis Trabajos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Destacadas.php">Trabajadores Destacados</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="Contacto.php">Contacto</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                <a href="inicioSesionE.php" class="btn btn-light btn-lg me-3">Iniciar Sesión</a>
+                <a href="Empresa-Usuario.php" class="btn btn-light btn-lg">Registro</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<br>
+<br>
+<br>
+<br>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 text-center mb-4">
+            <?php
+            if ($nombres) {
+                $gestion->drawNombre($nombres);
+            } else {
+                echo "<p>No se encontraron empresas asociadas al usuario.</p>";
+            }
+            ?>
+            <p>¡Bienvenido!</p>
+        </div>
+    </div>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-transparent mt-3">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="inicio.php"><img class="ml-4" style="height: 80px; width: 130px; margin-left: 50px;" src="img/LogoShortJobs-removebg-preview.png" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="menu">
-                <div class="d-flex ms-auto">
-                    <a href="inicioSesionE.php" class="btn btn-light btn-lg" style="margin-right: 15px;">Iniciar Sesión</a>
-                    <a href="Empresa-Usuario.php" class="btn btn-light btn-lg" style="margin-right: 60px;">Registro</a>
+    <div class="row">
+        <div class="col-12 text-center mb-4">
+            <img src="img/pruebaLogoEmpresa.png" alt="Logo de la Empresa" class="img-fluid" style="max-height: 200px;">
+        </div>
+    </div>
+    
+    <a href="formularioCrearTrabajo.php" class="btn btn-primary mb-4">Publicar Oferta</a>
+
+    <div class="row">
+        <?php
+        $trabajos->drawTrabajos($trabajoObstained);
+        ?>
+    </div>
+</div>
+
+<footer class="pie-pagina bg-dark text-light py-4 mt-5">
+    <div class="grupo-1 container">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="Inicio.php"><img src="img/LogoShortJobs-removebg-preview.png" alt="ShortJobs Logo" width="200px"></a>
+            </div>
+            <div class="col-md-4">
+                <h2>Contacto:</h2>
+                <p>Teléfono De Contacto: ☎️ 679 63 78 85</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Síguenos:</h2>
+                <div class="red-social">
+                    <a href="https://www.facebook.com/floridauniversitaria" class="fa fa-facebook" aria-label="Facebook"></a>
+                    <a href="https://www.instagram.com/florida_uni/" class="fa fa-instagram" aria-label="Instagram"></a>
+                    <a href="https://twitter.com/floridauni" class="fa fa-twitter" aria-label="Twitter"></a>
+                    <a href="https://www.linkedin.com/school/florida-universitaria/?originalSubdomain=es" class="fa fa-linkedin" aria-label="LinkedIn"></a>
+                    <a href="https://www.youtube.com/c/floridauniversitaria" class="fa fa-youtube" aria-label="YouTube"></a>
                 </div>
             </div>
         </div>
-    </nav>
+    </div>
+    <div class="grupo-2 container text-center mt-3">
+        <small>&copy; 2024 <b>ShortJobs</b> - Todos Los Derechos Reservados.</small>
+    </div>
+</footer>
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-12 text-center mb-4">
-                <?php
-                if ($nombres) {
-                    $gestion->drawNombre($nombres);
-                } else {
-                    echo "<p>No se encontraron empresas asociadas al usuario.</p>";
-                }
-                ?>
-                <?php
-                echo "<p>¡Bienvenido! </p>";
-                ?>
-
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12 text-center mb-4">
-                <img src="img/LogoEmpresa.png" alt="Logo de la Empresa" class="img-fluid" style="max-height: 200px;">
-            </div>
-        </div>
-        
-        <a href="formularioCrearTrabajo.php">
-        <button>Publicar Oferta</button></a>
-
-        <div class="row"  >
-            <?php
-            
-            $trabajos->drawTrabajos($trabajoObstained);
-            ?>
-        </div>
-    
-
-    <footer class="bg-dark text-light py-4">
-        <div class="container">
-           
-        </div>
-    </footer>
-
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
